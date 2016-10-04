793823a1f881a31d12606f8aafb918af1a807b28
Nick Sagona <nick@nolainteractive.com>
2016-10-03 21:26:05 -0500

    Rework the unit tests

--- tests/DirTest.php
+++ tests/DirTest.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Pop\File\Test;
+namespace Pop\Dir\Test;
 
-use Pop\File\Dir;
+use Pop\Dir\Dir;
 
 class DirTest extends \PHPUnit_Framework_TestCase
 {
@@ -60,16 +60,22 @@ class DirTest extends \PHPUnit_Framework_TestCase
             'recursive' => true,
             'filesOnly' => false
         ]);
-        $this->assertInstanceOf('Pop\File\Dir', $dir);
+        $this->assertInstanceOf('Pop\Dir\Dir', $dir);
         $this->assertEquals(__DIR__ . '/tmp', $dir->getPath());
+        $this->assertEquals(3, count($dir));
         $this->assertEquals(3, count($dir->getFiles()));
-        $this->assertEquals(3, count($dir->getObjects()));
         $this->assertEquals(1, count($dir->getTree()));
+
+        $c = [];
+        foreach ($dir as $file) {
+            $c[] = $file;
+        }
+        $this->assertEquals(3, count($c));
     }
 
     public function testConstructorDoesNotExistException()
     {
-        $this->setExpectedException('Pop\File\Exception');
+        $this->expectException('Pop\Dir\Exception');
         $dir = new Dir(__DIR__ . '/bad');
     }
 
@@ -106,16 +112,49 @@ class DirTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($dir->isRelative());
     }
 
-    public function testCopyDir()
+    public function testCopyTo()
     {
         mkdir(__DIR__ . '/copy');
         $dir = new Dir(__DIR__ . '/tmp');
-        $dir->copyDir(__DIR__ . '/copy');
+        $dir->copyTo(__DIR__ . '/copy');
         $this->assertFileExists(__DIR__ . '/copy/tmp');
 
         $dir = new Dir(__DIR__ . '/copy');
         $dir->emptyDir(true);
         $this->assertFileNotExists(__DIR__ . '/copy');
+    }
+
+    public function testOffsets()
+    {
+        $dir = new Dir(__DIR__ . '/tmp/', [
+            'relative'  => true,
+            'recursive' => true,
+            'filesOnly' => false
+        ]);
+        $this->assertTrue(isset($dir[0]));
+        $this->assertTrue(is_string($dir[0]));
+    }
+
+    public function testOffsetSetException()
+    {
+        $this->expectException('Pop\Dir\Exception');
+        $dir = new Dir(__DIR__ . '/tmp/', [
+            'relative'  => true,
+            'recursive' => true,
+            'filesOnly' => false
+        ]);
+        $dir[5] = 'tmp/file.txt';
+    }
+
+    public function testOffsetUnsetException()
+    {
+        $this->expectException('Pop\Dir\Exception');
+        $dir = new Dir(__DIR__ . '/tmp/', [
+            'relative'  => true,
+            'recursive' => true,
+            'filesOnly' => false
+        ]);
+        unset($dir[0]);
     }
 
 }
